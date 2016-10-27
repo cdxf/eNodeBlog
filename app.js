33736a43525591efb17c5db6532a0675608cb32e
Snoobvn <snoobvn@gmail.com>
2016-10-27 20:33:59 +0700

    hehe

--- app.js
+++ app.js
@@ -1,53 +1,33 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
-let count = 0;
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-	count++;
-    url += 'index.html';
-  }
-
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
-
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url == '/info/gen' || url == '/info/poll') {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-	let info = sysInfo[url.slice(6)]();
-	info.push({
-		 name: 'Count',
-		value: count
-	});
-    res.end(JSON.stringify(info));
-  } else {
-	if(url === '/loaderio-8eb8f8379effa193a04f5d801b49adc2/')
-		res.end('loaderio-8eb8f8379effa193a04f5d801b49adc2');
-	else{
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end('Not found');
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
-    });
-	}
-  }
+/**
+ * Created by Snoob on 9/29/2016.
+ */
+let [express,http,range,path,logger,bodyParser] =
+    [require("express"),require("http"),require("range"),require("path"),require("morgan"),require("body-parser")];
+let env = process.env;
+let app = express();
+let entries = [];
+app.locals.entries = entries;
+app.locals.range = range;
+app.set("views", path.resolve(__dirname, "views"));
+app.use(express.static('public'));
+app.set("view engine", "pug");
+app.use(logger("tiny"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/", function(request, response) {
+    reqLocal = response.locals;
+    reqLocal.pageInfo =  {};
+    reqLocal.pageInfo.title = "Snoob's Blog";
+    reqLocal.active = false;
+    response.render("index");
 });
-
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-  console.log(`Application worker ${process.pid} started...`);
+app.use(function(request, response) {
+    reqLocal = response.locals;
+    reqLocal.pageInfo =  {};
+    reqLocal.pageInfo.title = "404 Error";
+    reqLocal.active = false;
+    response.status(404).render("404");
+});
+var server = http.createServer(app);
+server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function() {
+    console.log(`Blog started on port ${env.NODE_PORT || 3000}.`)
 });
